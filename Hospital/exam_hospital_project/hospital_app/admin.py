33c6db5656bd31622eacda835a3fb80b2f6e81c3
Trajkow <98947729+Trajkow@users.noreply.github.com>
2026-06-01 19:00:44 +0200

    Initial commit: Added exercise Hospital

---
+++ Hospital/exam_hospital_project/hospital_app/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+from django.db.models import Q
+from .models import Doctor, Patient, Appointment, AppointmentAssignment
+
+# Register your models here.
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ("name", "last_name", "speciality", "institution", "num_completed_appointments", "email", "phone")
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("name", "last_name", "birth_date", "gender", "email")
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("type", "symptom_description", "status", "datetime", "patient", "responsible_doctor")
+
+    def has_add_permission(self, request):
+        return Doctor.objects.filter(user=request.user).exists() or request.user.is_superuser
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            if obj.responsible_doctor_id:
+                doctor = Doctor.objects.filter(user=request.user).first()
+                if doctor: obj.responsible_doctor_id = doctor.id
+        else:
+            old = Appointment.objects.get(pk=obj.pk)
+            if old.status == 'in_progress' and obj.status == 'completed':
+                obj.responsible_doctor.completed_appointments += 1
+                obj.responsible_doctor.save()
+        super().save_model(request, obj, form, change)
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        else:
+            doctor = Doctor.objects.filter(user=request.user).first()
+            if doctor.exists():
+                return obj.responsible_doctor == doctor
+        return False
+
+    def has_deleted_permission(self, request, obj=None):
+        if obj and obj.status != 'scheduled':
+            return False
+        return super().has_delete_permission(request, obj)
+
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(AppointmentAssignment)
---
+++ Hospital/exam_hospital_project/hospital_app/models.py
@@ -0,0 +1,70 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Doctor(models.Model):
+
+    speciality_choices = [
+        ('cardiologist', 'Cardiologist'),
+        ('dermatologist', 'Dermatologist'),
+        ('neurologist', 'Neurologist')
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    speciality = models.CharField(max_length=100, null=True, blank=True, choices=speciality_choices)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    institution = models.CharField(max_length=100, null=True, blank=True)
+    num_completed_appointments = models.IntegerField(null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name} - {self.speciality}'
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
+
+
+class Appointment(models.Model):
+
+    appointment_type_choices = [
+        ('cardiology', 'Cardiology'),
+        ('dermatology', 'Dermatology'),
+        ('neurology', 'Neurology'),
+    ]
+
+    status_choices = [
+        ('scheduled', 'Scheduled'),
+        ('in_progress', 'In Progress'),
+        ('finished', 'Finished'),
+    ]
+
+    type = models.CharField(max_length=100, null=True, blank=True, choices=appointment_type_choices)
+    symptom_description = models.CharField(max_length=100, null=True, blank=True)
+    status = models.CharField(max_length=100, null=True, blank=True, choices=status_choices)
+    datetime = models.DateField(null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    patient = (models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True, blank=True))
+    responsible_doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='responsible_appointments')
+
+    def __str__(self):
+        return f'{self.type} {self.symptom_description} - {self.status}'
+
+class AppointmentAssignment(models.Model):
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('appointment', 'doctor')
+
+    def __str__(self):
+        return f'{self.appointment} - {self.doctor}'
